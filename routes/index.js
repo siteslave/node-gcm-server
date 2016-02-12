a0a6347579e377cc5a38dd98012a9bc1d8401491
Satit Rianpit <siteslave@msn.com>
2016-02-12 13:54:30 +0700

    init

---
+++ routes/index.js
@@ -0,0 +1,54 @@
+var express = require('express');
+var router = express.Router();
+var fse = require('fs-extra');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.post('/register', function (req, res, next) {
+  //var json = fse.readJsonSync('./ids.json');
+  console.log(req.body.regid);
+    var json = fse.readJsonSync('./ids.json');
+    json.forEach(function (v) {
+      if (v != req.body.regid) json.push(req.body.regid);
+    })
+
+  fse.writeJsonSync('./ids.json', json);
+  res.send({ok: true});
+});
+
+router.post('/send-msg', function (req, res, next) {
+
+  var gcm = require('node-gcm');
+  var fse = require('fs-extra');
+
+  var message = new gcm.Message();
+
+  message.addData('message', 'ทดสอบส่งข้อความด้วย GCM');
+  message.addData('title', 'ประกาศ');
+
+  var ids = fse.readJsonSync('./ids.json');
+
+  var regTokens = ids;
+
+  // Set up the sender with you API key
+  var sender = new gcm.Sender('AIzaSyA_vHl6khNUwxIMSEPoLXywldPppvXlbDo');
+
+  // Now the sender can be used to send messages
+  sender.send(message, { registrationTokens: regTokens }, function (err, response) {
+      if(err) console.error(err);
+      else    console.log(response);
+  });
+
+  // Send to a topic, with no retry this time
+  sender.sendNoRetry(message, { topic: '/topics/global' }, function (err, response) {
+      if(err) console.error(err);
+      else    console.log(response);
+  });
+
+
+});
+
+module.exports = router;
